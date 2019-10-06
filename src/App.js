342d820fe77164ac084d41a7ede2d79ec0f1dc3e
Yusef Richard <yusef.rick@gmail.com>
2019-10-05 23:18:02 -0300

    creation of the front end complete

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import React, { useState} from 'react';
-import api from './services/api';
+import React from 'react';
 import './App.css';
 
 import logo from './assets/logo.svg'
 
+import Routes from './routes';
+
 function App() {
-  const[email, setEmail] = useState('');
-
-  async function handleSubmit(event){
-    event.preventDefault();
-
-    const response = await api.post('/sessions', {
-      email: email
-    });
-
-    const { _id} = response.data;
-
-    localStorage.setItem('user', _id);
-  }
 
 
   return (
     <div className="container">
       <img src={logo} alt="air cnc"/>
-
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre grandes <strong>talentos</strong> para sua empresa.
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E_MAIL *</label>
-          <input 
-          type="email" 
-          id="email" 
-          placeholder="Seu melhor email"
-          value = {email}
-          onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
     
